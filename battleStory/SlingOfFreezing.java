832e35b91fc9385f8a19c9c0b2bd308579c7afc4
Adrian Brady <ajbrady999@protonmail.com>
2023-09-28 12:09:30 -0500

    Implement sling of freezing

--- battleStory/SlingOfFreezing.java
+++ battleStory/SlingOfFreezing.java
@@ -2,4 +2,30 @@ package battleStory;
 
 public class SlingOfFreezing extends Sling {
    //TODO: override specified methods with default values
+   
+    @Override
+    public double getHitBonus() {
+        return 0.2;
+    }
+
+    @Override
+    public int getDamage() {
+        return 25;
+    }
+
+    @Override
+    public String getName() {
+        return "Sling of freezing";
+    }
+
+    @Override
+    public boolean addItem(Entity entity) {
+        for (double dim : entity.getDimensions()) {
+            if (dim > 10.0) {
+                return false;
+            }
+        }
+        super.addItem(entity);
+        return true;
+    }
 }
