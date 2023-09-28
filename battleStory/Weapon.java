6eb105536fbb229e01f5881458be28e887ffb080
Adrian Brady <ajbrady999@protonmail.com>
2023-09-28 11:16:15 -0500

    add implementation for weapon

--- battleStory/Weapon.java
+++ battleStory/Weapon.java
@@ -1,6 +1,50 @@
 package battleStory;
 
 public abstract class Weapon extends Entity implements Attacker {
+
+    private int damage;
+    private double hitBonus;
+    private String hitMessage;
+    private String missMessage;
+
+    public void setDamage(int damage) {
+        this.damage = damage;
+    }
+
+    public int getDamage() {
+        return damage;
+    }
+
+    public void setHitBonus(double hitBonus) {
+        this.hitBonus = hitBonus;
+    }
+
+    public double getHitBonus() {
+        return hitBonus;
+    }
+
+    public void setHitMessage(String hitMessage) {
+        this.hitMessage = hitMessage;
+    }
+
+    public String getHitMessage() {
+        return hitMessage;
+    }
+
+    public void setMissMessage(String missMessage) {
+        this.missMessage = missMessage;
+    }
+
+    public String getMissMessage() {
+        return missMessage;
+    }
+
+
+    public boolean attack(Entity entityToAttack, double distanceInMeters) {
+       return entityToAttack.receiveAttack(this, distanceInMeters);
+    }
+
+
 	//TODO: add implemented attack method
 
 	//TODO: add non-implemented abstract methods from interface Attacker
