f58fd038ee13cf7a9a7e548fcc6308bbf31ce0a3
Adrian Brady <ajbrady999@protonmail.com>
2023-10-05 09:31:01 -0500

    add StatePopulations lab

---
+++ StatePopulations/StatePair.java
@@ -0,0 +1,31 @@
+package StatePopulations;
+
+public class StatePair <Type1 extends Comparable<Type1>, Type2 extends Comparable<Type2>> {
+   private Type1 value1;
+   private Type2 value2;
+
+   public StatePair(Type1 value1, Type2 value2) {
+       this.value1 = value1;
+       this.value2 = value2;
+   }
+
+   public Type1 getValue1() {
+       return value1;
+   }
+
+   public void setValue1(Type1 value1) {
+       this.value1 = value1;
+   }
+
+   public Type2 getValue2() {
+       return value2;
+   }
+
+   public void setValue2(Type2 value2) {
+       this.value2 = value2;
+   }
+
+   public void printInfo() {
+       System.out.println();
+   }
+}
