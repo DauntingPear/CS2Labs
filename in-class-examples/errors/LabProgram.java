b975d66e7b22daf0eb21e9f44f32d7a291cc65fc
Adrian Brady <ajbrady999@protonmail.com>
2023-10-24 10:44:50 -0500

    add initial example which can error
    can error if user inputs value other than int

---
+++ in-class-examples/errors/LabProgram.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class LabProgram {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int input = scan.nextInt();
+
+        System.out.println(input);
+    }
+}
