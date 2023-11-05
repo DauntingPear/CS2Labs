458893af3f78a6033b1a4c489e765698bdf4d60d
Adrian Brady <ajbrady999@protonmail.com>
2023-11-05 15:46:58 -0600

    finish steps 1/2

--- Data Visualization/DataVisualizer.java
+++ Data Visualization/DataVisualizer.java
@@ -5,7 +5,17 @@ public class DataVisualizer {
    public static void main(String[] args) {
       Scanner scnr = new Scanner(System.in);
 
-      /* Type your code here. */
+      String col1h;
+      String col2h;
+
+      System.out.println("Enter column 1 header:");
+      col1h = scnr.nextLine();
+      System.out.println("You entered: " + col1h);
+
+      System.out.println("Enter column 2 header:");
+      col2h = scnr.nextLine();
+      System.out.println("You entered: " + col2h);
+
    }
 }
 
