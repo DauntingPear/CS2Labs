7494cc044593340b7a137b0f46687a9956159baa
Adrian Brady <ajbrady999@protonmail.com>
2023-11-05 17:41:45 -0600

    implement initial file reading

--- Movie show time display/LabProgram.java
+++ Movie show time display/LabProgram.java
@@ -5,6 +5,19 @@ import java.io.IOException;
 public class LabProgram {
    public static void main(String[] args) throws IOException {
       Scanner scnr = new Scanner(System.in);
+      FileInputStream fileByteStream = null;
+      Scanner inFS = null;
+      String fileLine;
+
+      fileByteStream = new FileInputStream("movies.csv");
+      inFS = new Scanner(fileByteStream);
+
+      while (inFS.hasNextLine()) {
+          fileLine = inFS.nextLine();
+          System.out.println("Line: " + fileLine);
+      }
+
+      fileByteStream.close();
 
       /* Type your code here. */
    }
