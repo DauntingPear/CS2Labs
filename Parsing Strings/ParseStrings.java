55185fd5a7b60e378f1b5cc9430844742242a67a
Adrian Brady <ajbrady999@protonmail.com>
2023-11-05 15:11:49 -0600

    create initial lab environment

---
+++ Parsing Strings/ParseStrings.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class ParseStrings {
+   public static void main(String[] args) {
+      Scanner scnr = new Scanner(System.in);
+
+      /* Type your code here. */
+   }
+}
+
