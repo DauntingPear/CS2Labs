3e465083d20eef2761944acaddf039b16d5fb4ed
Adrian Brady <ajbrady999@protonmail.com>
2023-11-05 15:39:18 -0600

    add initial lab starting point

---
+++ Data Visualization/DataVisualizer.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+
+public class DataVisualizer {
+   public static void main(String[] args) {
+      Scanner scnr = new Scanner(System.in);
+
+      /* Type your code here. */
+   }
+}
+
