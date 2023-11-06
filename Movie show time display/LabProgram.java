0bc74261223d81b275ef70ac01a5feddbd4d4b51
Adrian Brady <ajbrady999@protonmail.com>
2023-11-05 18:36:55 -0600

    add more

--- Movie show time display/LabProgram.java
+++ Movie show time display/LabProgram.java
@@ -3,23 +3,23 @@ import java.io.FileInputStream;
 import java.io.IOException;
 
 public class LabProgram {
-   public static void main(String[] args) throws IOException {
-      Scanner scnr = new Scanner(System.in);
-      FileInputStream fileByteStream = null;
-      Scanner inFS = null;
-      String fileLine;
+    public static void main(String[] args) throws IOException {
+        Scanner scnr = new Scanner(System.in);
+        FileInputStream fileByteStream = null;
+        Scanner inFS = null;
+        String fileLine;
+        String[] lineParts;
 
-      fileByteStream = new FileInputStream("movies.csv");
-      inFS = new Scanner(fileByteStream);
+        fileByteStream = new FileInputStream("movies.csv");
+        inFS = new Scanner(fileByteStream);
 
-      while (inFS.hasNextLine()) {
-          fileLine = inFS.nextLine();
-          System.out.println("Line: " + fileLine);
-      }
+        while (inFS.hasNextLine()) {
+            fileLine = inFS.nextLine();
+            System.out.println("Line: " + fileLine);
+            lineParts = fileLine.split(",");
+        }
 
-      fileByteStream.close();
-
-      /* Type your code here. */
-   }
+        fileByteStream.close();
+    }
 }
 
