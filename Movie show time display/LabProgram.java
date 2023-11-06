d104223e1ca466f7e2ea5234aa7cc633b33df8cb
Adrian Brady <ajbrady999@protonmail.com>
2023-11-05 20:52:59 -0600

    finish lab

--- Movie show time display/LabProgram.java
+++ Movie show time display/LabProgram.java
@@ -1,25 +1,55 @@
 import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileInputStream;
 import java.io.IOException;
 
 public class LabProgram {
     public static void main(String[] args) throws IOException {
         Scanner scnr = new Scanner(System.in);
-        FileInputStream fileByteStream = null;
-        Scanner inFS = null;
-        String fileLine;
-        String[] lineParts;
 
-        fileByteStream = new FileInputStream("movies.csv");
-        inFS = new Scanner(fileByteStream);
+        String fileName = scnr.next();
 
-        while (inFS.hasNextLine()) {
-            fileLine = inFS.nextLine();
-            System.out.println("Line: " + fileLine);
-            lineParts = fileLine.split(",");
+        File file = new File(fileName);
+
+        try(FileInputStream fis = new FileInputStream(file); Scanner fileScan = new Scanner(fis);) {
+            String times = "";
+            String lastTitle = "";
+            String lastRating = "";
+
+            while (fileScan.hasNext()) {
+                String line = fileScan.nextLine();
+                //System.out.println(line);
+
+                String[] tokens = line.split(",");
+                
+                String title = tokens[1];
+                String rating = tokens[2];
+                //System.out.println(title + "<>" + rating);
+
+                if(title.length() > 44) title = title.substring(0, 44);
+
+                if (!lastTitle.equals("") && !title.equals(lastTitle)) {
+                    System.out.printf("%-44s | %5s | %s\n", lastTitle, lastRating, times);
+                    times = "";
+                }
+
+                lastRating = rating;
+                lastTitle = title;
+
+                if (!times.equals("")) times += " ";
+                times += tokens[0];
+            }
+
+            System.out.printf("%-44s | %5s | %s\n", lastTitle, lastRating, times);
+        }
+        catch (FileNotFoundException ex) {
+            //handle
+        }
+        catch (IOException ex) {
+            //handle
         }
 
-        fileByteStream.close();
     }
 }
 
