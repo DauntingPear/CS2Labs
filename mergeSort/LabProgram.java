98d3adbd4ce8aac2d9a15ff703ec331d559a2d13
Adrian Brady <ajbrady999@protonmail.com>
2023-12-10 11:29:28 -0600

    fix indentation

--- mergeSort/LabProgram.java
+++ mergeSort/LabProgram.java
@@ -1,96 +1,96 @@
 import java.util.Scanner;
 
 public class LabProgram {
-   // Read size and create an array of size ints.
-   // Read size ints, storing them in the array.
-   // Return the array.
-   public static int[] readNums() {
-      Scanner scnr = new Scanner(System.in);
-      int size = scnr.nextInt();                  // Read array size
-      int[] nums = new int[size];                 // Create array
-      for (int j = 0; j < size; ++j) {            // Read the numsbers
-         nums[j] = scnr.nextInt();
-      }
-      return nums;                                // Return the array
-   }
+    // Read size and create an array of size ints.
+    // Read size ints, storing them in the array.
+    // Return the array.
+    public static int[] readNums() {
+        Scanner scnr = new Scanner(System.in);
+        int size = scnr.nextInt();                  // Read array size
+        int[] nums = new int[size];                 // Create array
+        for (int j = 0; j < size; ++j) {            // Read the numsbers
+            nums[j] = scnr.nextInt();
+        }
+        return nums;                                // Return the array
+    }
 
-   // Output the numbers in nums, separated by spaces.
-   // No space or newline will be output before the first number or after the last.
-   public static void printNums(int[] nums) {
-      for (int i = 0; i < nums.length; i++) {
-         System.out.print(nums[i]);
-         if (i < nums.length) {
-            System.out.print(" ");
-         }
-      }
-   }
+    // Output the numbers in nums, separated by spaces.
+    // No space or newline will be output before the first number or after the last.
+    public static void printNums(int[] nums) {
+        for (int i = 0; i < nums.length; i++) {
+            System.out.print(nums[i]);
+            if (i < nums.length) {
+                System.out.print(" ");
+            }
+        }
+    }
 
-   public static void merge(int[] numbers, int i, int j, int k) {
-      int mergedSize = k - i + 1;
-      int mergedNumbers[] = new int[mergedSize];
-      int mergePos;
-      int leftPos;
-      int rightPos;
+    public static void merge(int[] numbers, int i, int j, int k) {
+        int mergedSize = k - i + 1;
+        int mergedNumbers[] = new int[mergedSize];
+        int mergePos;
+        int leftPos;
+        int rightPos;
 
-      mergePos = 0;
-      leftPos = i;
-      rightPos = j + 1;
+        mergePos = 0;
+        leftPos = i;
+        rightPos = j + 1;
 
-      while (leftPos <= j && rightPos <= k) {
-         if (numbers[leftPos] < numbers[rightPos]) {
+        while (leftPos <= j && rightPos <= k) {
+            if (numbers[leftPos] < numbers[rightPos]) {
+                mergedNumbers[mergePos] = numbers[leftPos];
+                ++leftPos;
+            }
+            else {
+                mergedNumbers[mergePos] = numbers[rightPos];
+                ++rightPos;
+            }
+            ++mergePos;
+        }
+
+        while (leftPos <= j) {
             mergedNumbers[mergePos] = numbers[leftPos];
             ++leftPos;
-         }
-         else {
+            ++mergePos;
+        }
+
+        while (rightPos <= k) {
             mergedNumbers[mergePos] = numbers[rightPos];
             ++rightPos;
-         }
-         ++mergePos;
-      }
+            ++mergePos;
+        }
 
-      while (leftPos <= j) {
-         mergedNumbers[mergePos] = numbers[leftPos];
-         ++leftPos;
-         ++mergePos;
-      }
+        for (mergePos = 0; mergePos < mergedSize; ++mergePos) {
+            numbers[i + mergePos] = mergedNumbers[mergePos];
+        }
+    }
 
-      while (rightPos <= k) {
-         mergedNumbers[mergePos] = numbers[rightPos];
-         ++rightPos;
-         ++mergePos;
-      }
+    public static void mergeSort(int numbers[], int i, int k) {
+        int j;
 
-      for (mergePos = 0; mergePos < mergedSize; ++mergePos) {
-         numbers[i + mergePos] = mergedNumbers[mergePos];
-      }
-   }
+        if (i < k) {
+            j = (i + k) / 2;
+            /* Trace output added to code in book */
+            System.out.printf("%d %d | %d %d\n", i, j, j+1, k);
 
-   public static void mergeSort(int numbers[], int i, int k) {
-      int j;
+            mergeSort(numbers, i, j);
+            mergeSort(numbers, j + 1, k);
 
-      if (i < k) {
-         j = (i + k) / 2;
-         /* Trace output added to code in book */
-         System.out.printf("%d %d | %d %d\n", i, j, j+1, k);
+            merge(numbers, i, j, k);
+        }
+    }
 
-         mergeSort(numbers, i, j);
-         mergeSort(numbers, j + 1, k);
+    public static void  main(String[] args) {
+        int[] numbers = readNums();
 
-         merge(numbers, i, j, k);
-      }
-   }
+        System.out.print("unsorted: ");
+        printNums(numbers);
+        System.out.println("\n");
 
-   public static void  main(String[] args) {
-      int[] numbers = readNums();
+        mergeSort(numbers, 0, numbers.length - 1);
 
-      System.out.print("unsorted: ");
-      printNums(numbers);
-      System.out.println("\n");
-
-      mergeSort(numbers, 0, numbers.length - 1);
-
-      System.out.print("\nsorted:   ");
-      printNums(numbers);
-   }
+        System.out.print("\nsorted:   ");
+        printNums(numbers);
+    }
 }
 
